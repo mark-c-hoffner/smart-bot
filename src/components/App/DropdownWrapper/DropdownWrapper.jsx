93140bad8abfa93c62765e2e9a18504690d96343
Mark Hoffner <mark.c.hoffner@gmail.com>
2023-05-01 12:26:56 -0400

    STYLING: Style dropdown selector

--- src/components/App/DropdownWrapper/DropdownWrapper.jsx
+++ src/components/App/DropdownWrapper/DropdownWrapper.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Select from "react-select";
+import "./dropdown-wrapper.css";
 
 /**
  * React Function Component wraps the dropdown library and prepares color list for dropdown display.
@@ -22,8 +23,62 @@ const DropdownWrapper = ({ imageItem, colorItem, colors, handleDropdownChange })
         return options;
     };
 
+    const rgb = (r, g, b) => {
+        return `rgb(${r}, ${g}, ${b})`;
+    }
+
+    const selectStyling = {
+        control: (baseStyles, state) => ({
+            ...baseStyles,
+            textAlign: "center",
+            border: "none",
+            cursor: "pointer",
+            boxShadow: "inset 0em 1.9em #161616",
+            background: "radial-gradient(rgb(255, 255, 255), #161616)",
+            minHeight: "0px",
+            height: "2.1em",
+            paddingBottom: "0px",
+        }),
+        placeholder: (baseStyles, state) => ({
+            ...baseStyles,
+            fontSize: "1.4em",
+            color: rgb(213, 213, 213),
+            "&:hover": {
+                color: "white",
+            },
+        }),
+        singleValue: (baseStyles, state) => ({
+            ...baseStyles,
+            fontSize: "1.4em",
+            color: rgb(213, 213, 213),
+            "&:hover": {
+                color: "white",
+            },
+        }),
+        menu: (baseStyles, state) => ({
+            ...baseStyles,
+            backgroundColor: rgb(50, 50, 50),
+            color: rgb(230, 230, 230),
+        }),
+        option: (baseStyles, state) => ({
+            ...baseStyles,
+            cursor: "pointer",
+            backgroundColor: state.isFocused ? rgb(60, 60, 60) : rgb(50, 50, 50),
+            color: state.isFocused ? rgb(255, 255, 255) : rgb(230, 230, 230),
+            fontSize: state.isFocused ? '1.1em': '1em'
+        }),
+    }
+
     return (
-        <Select options={getDropdownDisplayItems(colors)} onChange={e => handleDropdownChange(imageItem, colorItem, e)} placeholder={colorItem.name} />
+        <Select
+            className="dropdown-container"
+            styles={selectStyling}
+            isSearchable={false}
+            isClearable={false}
+            placeholder={colorItem.name}
+            options={getDropdownDisplayItems(colors)}
+            onChange={e => handleDropdownChange(imageItem, colorItem, e)}
+        />
     );
 };
 
